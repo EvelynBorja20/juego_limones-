586afdaffbe27a89219bffdcc2c534634712f924
EvelynBorja20 <tatisborjalinda2018@gmail.com>
2026-04-14 16:09:28 -0500

    agregar dibujarSuelo y Dibujar personaje

--- limones.js
+++ limones.js
@@ -1,2 +1,17 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+const ALTURA_SUELO=20
+}
+function iniciar(){
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+function dibujarSuelo(){
+    ctx.fillStyle="blue"
+    ctx.fillRect(0,canvas.height-alturaSuelo,canvas.width,40);
+}
+
+function dibujarPersonaje(){
+    ctx.fillStyle="yellow"
+    ctx.fillRect(canvas.width/2,canvas.height-(4+60),40,60)
+}

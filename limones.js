16feb31a0da0b77ff1c10b50fe5f8f1d633a7340
EvelynBorja20 <tatisborjalinda2018@gmail.com>
2026-04-15 14:29:48 -0500

    mover a la izquierda y a la derecha

--- limones.js
+++ limones.js
@@ -1,17 +1,39 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
-const ALTURA_SUELO=20
-}
+
+const ALTURA_SUELO=20;
+const ALTURA_PERSONAJE=60;
+const ANCHO_PERSONAJE=40;
+let personajeX=canvas.width/2;
+
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
 }
 function dibujarSuelo(){
     ctx.fillStyle="blue"
-    ctx.fillRect(0,canvas.height-alturaSuelo,canvas.width,40);
+    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
 }
 
 function dibujarPersonaje(){
     ctx.fillStyle="yellow"
-    ctx.fillRect(canvas.width/2,canvas.height-(4+60),40,60)
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarPantalla();
+}
+   //mover derecha
+function moverDerecha(){
+    personajeX=personajeX+10;
+    actualizarPantalla();
+}
+function actualizarPantalla(){
+    limpiarCanva()
+    dibujarSuelo()
+    dibujarPersonaje();
+}
+
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }

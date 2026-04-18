a68a9cdc00498d7a8e94de8cf36ca26283c0c562
EvelynBorja20 <tatisborjalinda2018@gmail.com>
2026-04-17 19:22:48 -0500

    agregar coliciones

--- limones.js
+++ limones.js
@@ -8,8 +8,9 @@ const ANCHO_LIMON=20
 const ALTURA_LIMON=20
 
 let personajeX=canvas.width/2;
+let personajeY=canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
 let limonX=canvas.width/2;
-let limonY=10;
+let limonY=0;
 
 function iniciar(){
     dibujarSuelo();
@@ -23,16 +24,18 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle="yellow"
-    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,personajeY,ANCHO_PERSONAJE,ALTURA_PERSONAJE);
 }
 function moverIzquierda(){
     personajeX=personajeX-10;
     actualizarPantalla();
+    detectarColision();
 }
    //mover derecha
 function moverDerecha(){
     personajeX=personajeX+10;
     actualizarPantalla();
+    detectarColision();
 }
 function actualizarPantalla(){
     limpiarCanva();
@@ -51,4 +54,13 @@ function dibujarLimon(){
 function bajarLimon(){
     limonY = limonY+10;
     actualizarPantalla();
+}
+
+function detectarColision(){
+    if(limonX+ANCHO_LIMON>personajeX &&
+        limonX < personajeX+ANCHO_PERSONAJE &&
+        limonY + ALTURA_LIMON > personajeY &&
+        limonY < personajeY+ALTURA_PERSONAJE){
+        alert("ATRAPADO !!")
+    }
 }
